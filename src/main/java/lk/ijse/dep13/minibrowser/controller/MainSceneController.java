a06d3cdd77992f73e4a8b062fceb821c8e8ac78e
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-01-25 22:59:37 +0530

    Design UI part

--- src/main/java/lk/ijse/dep13/minibrowser/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/minibrowser/controller/MainSceneController.java
@@ -1,4 +1,18 @@
 package lk.ijse.dep13.minibrowser.controller;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+
 public class MainSceneController {
+
+    public AnchorPane root;
+    public TextField txtAddress;
+    public WebView wbDisplay;
+
+    public void txtAddressOnAction(ActionEvent event) {
+
+    }
 }

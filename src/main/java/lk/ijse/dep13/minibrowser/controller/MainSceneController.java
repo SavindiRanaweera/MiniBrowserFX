ef135800004430b466075ffb786efe9b6dd72575
Savindi Ranaweera <savindi.ch1995@gmail.com>
2025-01-26 00:51:18 +0530

    Update MainSceneController: Separate the host and port and Identify the path

--- src/main/java/lk/ijse/dep13/minibrowser/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/minibrowser/controller/MainSceneController.java
@@ -38,16 +38,44 @@ public class MainSceneController {
         int port = -1;
         String path = null;
 
-        //Identify the protocol
-        if((i = url.indexOf ( "://" )) != -1){
-            protocol = url.substring (0, i );
-            url = url.substring (i + 3);
+        try{
+            //Identify the protocol
+            if((i = url.indexOf ( "://" )) != -1){
+                protocol = url.substring (0, i );
+                url = url.substring (i + 3);
+            }
+
+            //Identify the host and port
+            int slashIndex = url.indexOf ( "/" );
+            String hostNPort = (slashIndex != -1) ? url.substring (0, slashIndex) : url;
+            path = (slashIndex != -1) ? url.substring (slashIndex ) : url ;
+
+            //Separate the host and port
+            int colonIndex = hostNPort.indexOf ( ":" );
+            if(colonIndex != -1){
+                host = hostNPort.substring (0, colonIndex);
+                try{
+                    port = Integer.parseInt(hostNPort.substring(colonIndex+1));
+                }catch (NumberFormatException e){
+                    throw new RuntimeException ("Invalid port number");
+                }
+            }else {
+                host = hostNPort;
+                port = (protocol != null && protocol.equalsIgnoreCase ( "https" )) ?443 :
+                        (protocol != null && protocol.equalsIgnoreCase ( "http" )) ? 80 :
+                                (protocol == null) ? 80 : -1;
+            }
+            if(protocol == null) protocol = "http";
+
+            System.out.println ("Protocol: " + protocol );
+            System.out.println ("Host: " + host );
+            System.out.println ("Port: " + port );
+            System.out.println ("Path: " + path );
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
-        //Identify the host and port
-        int slashIndex = url.indexOf ( "/" );
-        String hostNPort = (slashIndex != -1) ? url.substring (0, slashIndex) : url;
-        path = (slashIndex != -1) ? url.substring (slashIndex ) : url ;
 
     }
 }
